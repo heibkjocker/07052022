5479d3c081a9ed6b78a7576afb95c39aca56521a
Cristian Quintero <cristian.quinterom94@hotmail.com>
2022-07-02 20:12:15 -0500

    Curso MINTIC python

---
+++ versionVectores.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from tkinter import messagebox
+import re
+
+v = Tk()
+
+Label(v, text="Numero decimal").grid(row=0)
+txtD = Entry(v, width=10)
+txtD.grid(row=0, column=1)
+
+txtH = Entry(v, width=20)
+txtH.grid(row=1, column=1)
+txtH.configure(state=DISABLED)
+
+
+def ObtenerDigitoHexadecimal(valorDecimal):
+    if valorDecimal <= 15:
+        letras = ("A", "B", "C", "D", "E", "F")
+        if valorDecimal >= 10:
+            digitoHexadecimal = letras[valorDecimal-10]
+        else:
+            digitoHexadecimal = str(valorDecimal)
+    else:
+        digitoHexadecimal = ""
+    return digitoHexadecimal
+
+
+def Calcular():
+    # Validar datos de entrada
+    if re.match("^[0-9]+$", txtD.get()):
+        # Obtener datos de entrada
+        d = int(txtD.get())
+
+        # Proceso
+        h = ""
+        while d >= 16:
+            # Obtener el digito hexadecimal
+            dh = d % 16
+            h = ObtenerDigitoHexadecimal(dh) + h
+            # d = int((d - dh) / 16)
+            d = d // 16
+
+            h = ObtenerDigitoHexadecimal(d) + h
+
+        # mostrar resultados
+        txtH.configure(state=NORMAL)
+        txtH.delete(0, END)
+        txtH.insert(0, h)
+        txtH.configure(state=DISABLED)
+
+    else:
+        messagebox.showerror("", "El dato debe ser un numero entero")
+
+
+Button(v, text="Numero Hexadecimal", command=Calcular).grid(row=1)
+v.mainloop()
